e242cd5dfd051ee6cfb033a294394df4c1e23719
Lucas Vinícius Carmassi <carmassilucas@gmail.com>
2022-12-28 17:16:38 -0300

    challenge solution

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,19 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    if (!email || !name) {
+      throw new Error("Unprocessable Entity");
+    }
+
+    const userAlteradyExists = this.usersRepository.findByEmail(email);
+
+    if (userAlteradyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
